c6c37d3d7b3dbf84c199304c575b2b6d947384fe
lucas <lucas@techforb.com>
2024-08-14 01:33:17 -0300

    2

--- src/app/components/AdSense.js
+++ src/app/components/AdSense.js
@@ -5,7 +5,7 @@ const AdSense = () => {
       async
       src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=pub-8737844615082936'
       crossOrigin="anonymous"
-      strategy="afterInteractive"
+      strategy="lazyOnload"
       />
     )
 };
