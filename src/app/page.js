53920de4aae408144b775fb7d479f0cccb30e3d4
lucas <lucas@techforb.com>
2024-08-14 14:15:21 -0300

    321

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,8 @@
 import AdBanner from './components/AdBanner';
 import Cotizaciones from './components/Cotizaciones/Cotizaciones';
 import LiveVideo from './components/LiveVideo';
-const channelIds = ['UCeY0bbntWzzVIaj2z3QigXg'] // Reemplaza con tus IDs de canal
+
+const channelIds = ['UCWSfXECGo1qK_H7SXRaUSMg'] // Reemplaza con tus IDs de canal
 
 export default function Page () {
   return (
